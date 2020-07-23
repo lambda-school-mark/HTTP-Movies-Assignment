d96fee0be13605a0962b26ccb44d760aaf544292
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-07-23 11:08:55 -0400

    finished and bugs fixed

--- client/src/App.js
+++ client/src/App.js
@@ -3,38 +3,61 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-import axios from 'axios';
+import axios from "axios";
+import UpdateMovie from "./Movies/UpdateMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
 
-  const getMovieList = () => {
-    axios
-      .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
-      .catch(err => console.log(err.response));
-  };
-
-  const addToSavedList = movie => {
+  const addToSavedList = (movie) => {
     setSavedList([...savedList, movie]);
   };
 
   useEffect(() => {
-    getMovieList();
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then((res) => setMovieList(res.data))
+      .catch((err) => console.log(err.response));
   }, []);
-
   return (
     <>
       <SavedList list={savedList} />
 
-      <Route exact path="/">
-        <MovieList movies={movieList} />
-      </Route>
+      <Route
+        exact
+        path="/"
+        render={(props) => (
+          <MovieList
+            {...props}
+            movieList={movieList}
+            setMovieList={setMovieList}
+          />
+        )}
+      />
 
-      <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
-      </Route>
+      <Route
+        path="/movies/:id"
+        render={(props) => (
+          <Movie
+            {...props}
+            addToSavedList={addToSavedList}
+            movieList={movieList}
+            setMovieList={setMovieList}
+          />
+        )}
+      />
+
+      <Route
+        path="/update-movie/:id"
+        render={(props) => (
+          <UpdateMovie
+            {...props}
+            movieList={movieList}
+            setMovieList={setMovieList}
+          />
+        )}
+      />
     </>
   );
 };
